9f0ef749e4da39cfca0e365ca3c47f64a1433227
juanBustosEspinosa <suertenani@gmail.com>
2025-05-19 14:57:56 +0200

    Footer

--- React/mundoVideojuegos/src/App.jsx
+++ React/mundoVideojuegos/src/App.jsx
@@ -18,6 +18,7 @@ import ComponentMostrarUsuario from './Component/Mensaje/BuscarUsuario/Component
 import ComponentJuegoDetallado from './Component/Juegos/DetallesJuego/ComponentJuegoDetallado';
 import ComponentPerfil from './Component/Mensaje/Perfil/ComponentPerfil';
 import ComponentCrearJuego from './Component/Juegos/CrearJuego/CompoenetCrearJuego';
+import ComponentFooter from './Component/Footer/ComponentFooter';
 
 function App() {
   const { isAuthenticated } = useContext(AuthContext); // Obtener el estado de autenticación desde el contexto
@@ -29,16 +30,23 @@ function App() {
         {/* Ruta para el login */}
         <Route path="/login" element={<ComponentLogin />} />
 
+                {/** Te envia al registro para que el usuario se cree su cuenta */}
+        <Route path='/registro' element=  {<ComponentRegistro/>} />
+
         {/* Ruta protegida: Si no está autenticado, redirige a /login */}
         <Route
           path="/formulario"
           element={isAuthenticated ? <p>Bienvenido al formulario</p> : <Navigate to="/login" />}
         />
 
+
+
+
         {/* Ruta principal: Solo accesible si estás autenticado */}
         <Route
           path="/"
-          element={isAuthenticated ?     <div className="pagina-contenedor">
+          element={isAuthenticated ?     
+          <div className="pagina-contenedor">
             {/* Menú fijo en la parte superior */}
             <div className="menu-fijo">
               <ComponentMenu />
@@ -47,73 +55,171 @@ function App() {
             {/* Contenedor para los mensajes o el contenido dinámico */}
             <div className="contenido-principal">
               <ComponentMensaje idjuego={null} idUsuario={null} />
+              <ComponetBusquedaUsuario />
             </div>
-            <ComponetBusquedaUsuario />
+            
 
             {/* Botón de "Publicar" fijo en la parte inferior */}
-            <ButtonPublicar className="publicar-fijo" />
+            <ButtonPublicar />
+            <div className='footer'>
+              <ComponentFooter />
+            </div>
           </div> : <Navigate to="/login" />}
         />
-        {/** Te envia al registro para que el usuario se cree su cuenta */}
-        <Route
-          path='/registro'
-          element=  {<ComponentRegistro/>}
-        />
+
+
+
+
         {/**Compoenete que te lleva al apartado general de los juegos */}
         <Route
         path='/juegos'
         element={isAuthenticated ?
+          <div className="pagina-contenedor">
+              <div className="menu-fijo">
+                <ComponentMenu />
+              </div> 
           <div className="contenedor-juegos">
-            <div className="menu-fijo">
-              <ComponentMenu />
-            </div>
+          
             <div className="contenido-juego">
               <ComponentJuego idUsuario={null} />
               <ComponentLectura/>
             </div>
+          </div>
+          <div className='footer'>
+              <ComponentFooter />
+          </div>
           </div>: <Navigate to="/login"/> }
         />
 
         {/** Boton que te permite publicar un mensaje sobre un juego */}
         <Route
         path='/publicacion'
-        element={isAuthenticated ? <div><ComponentMenu/> <ComponentPublicar/> </div> : <Navigate to="/login"/>}
+        element={isAuthenticated ?         
+         <div className="pagina-contenedor">
+            <div className="menu-fijo">
+              <ComponentMenu />
+            </div>  
+          <div className="contenido-principal-publicar">          
+            <ComponentPublicar/>             
+          </div>         
+          <div className='footer'>
+              <ComponentFooter />
+          </div>          
+          </div> : <Navigate to="/login"/>}
         />
 
         <Route
         path='/Responder'
-        element={isAuthenticated ? <div><ComponentMenu/>  <ComponentResponder/> </div> : <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">
+            <div className="menu-fijo">
+              <ComponentMenu />
+            </div>          
+            <div className="contenido-principal">
+              <ComponentResponder/>
+            </div>             
+            <div className='footer'>
+              <ComponentFooter />
+            </div>  
+          </div> : <Navigate to="/login"/>}
         />  
 
 
         <Route
         path="/EleccionJuego"
-        element={isAuthenticated ? <div><BusquedaSeleccionCompoenet /> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">
+            <div className="menu-fijo">
+              <ComponentMenu />
+            </div>
+          <div className="contenido-principal-busqueda">
+          <BusquedaSeleccionCompoenet /> 
+          </div>   
+          <div className='footer'>
+              <ComponentFooter />
+          </div>  </div>: <Navigate to="/login"/>}
         />
+
+
         <Route
         path="/Perfil"
-        element={isAuthenticated ? <div><ComponentMenu/> <ComponentPerfil /> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">
+            <div className="menu-fijo">
+              <ComponentMenu />
+            </div>          
+            <div className="contenido-principal-perfil">
+              <ComponentPerfil />  
+            </div>           
+          <div className='footer'>
+              <ComponentFooter />
+          </div>
+          </div>: <Navigate to="/login"/>}
         />
         
         
         {/**Esta ruta te lleva a detalles del juego */}
         <Route
         path="/DetalleJuego"
-        element={isAuthenticated ? <div>  <ComponentMenu/>   <ComponentJuegoDetallado /> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">        
+          <div className="menu-fijo">
+            <ComponentMenu />
+          </div>          
+          <div className="contenido-principal-detalleJuego">
+
+          <ComponentJuegoDetallado />          
+          </div>          
+
+          <div className='footer'>
+              <ComponentFooter />
+          </div>   
+           </div>: <Navigate to="/login"/>}
         />
+
+
+
           <Route
         path="/CrearJuego"
-        element={isAuthenticated ? <div>  <ComponentMenu/>   <ComponentCrearJuego /> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">         
+          <div className="menu-fijo">
+           <ComponentMenu />
+          </div>           
+          <div className="contenido-principal-detalleJuego"> {/**Revisar como ha quedado  */}
+            <ComponentCrearJuego />  
+          </div>          
+          <div className='footer'>
+              <ComponentFooter />
+          </div>
+          </div>: <Navigate to="/login"/>}
         />
+
 
         <Route
         path="/BusquedaJuego"
-        element={isAuthenticated ? <div> <ComponentMenu/> <CompoenetBusqueda/> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor">        
+          <div className="menu-fijo">
+            <ComponentMenu />
+          </div>      
+          <div className="contenido-principal-perfil">
+             <CompoenetBusqueda/>
+            </div>          
+             
+          <div className='footer'>
+              <ComponentFooter />
+          </div>             
+             </div>: <Navigate to="/login"/>}
         />
 
         <Route
         path="/BusquedaUsuario"
-        element={isAuthenticated ? <div> <ComponentMenu/> <ComponentMostrarUsuario/> </div>: <Navigate to="/login"/>}
+        element={isAuthenticated ? <div className="pagina-contenedor"> 
+          <div className="menu-fijo">
+            <ComponentMenu />
+          </div>        
+          <div className="contenido-principal-perfil">
+           <ComponentMostrarUsuario/>             
+          </div>
+          <div className='footer'>
+              <ComponentFooter />
+          </div>          
+          </div>: <Navigate to="/login"/>}
         />
 
 
